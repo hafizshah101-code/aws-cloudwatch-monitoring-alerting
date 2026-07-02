d9e7d48977e4cf0319ad7253a1cd5ef02162df9d
Hafiz Shah <hafiz.shah101@gmail.com>
2026-07-02 09:25:05 +0800

    Add files via upload

---
+++ install-cloudwatch-agent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum install amazon-cloudwatch-agent -y
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+-s
